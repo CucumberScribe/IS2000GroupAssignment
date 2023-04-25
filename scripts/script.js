2a76cc5ab50ecb60d54da8267026d05acf91a19f
CucumberScribe <aramishahne@gmail.com>
2023-04-24 20:25:42 -0600

    Finished Stories, contact, added js

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// Contributors: Aramis Hahne
+
+function showVid () {
+  var video = document.getElementById("vid")
+  if (video.style.display === "none") {
+    video.style.display = "block";
+  } else {
+    video.style.display = "none";
+  }
+}
