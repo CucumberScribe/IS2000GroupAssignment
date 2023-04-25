1770262eeb008371e16c1af16922e6717c164e4b
Wally810 <mwallyhoff@icloud.com>
2023-04-25 12:42:18 -0600

    Completed the Donations Page with Forms

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Contributors: Aramis Hahne
+// Contributors: Aramis Hahne, Michael Hoff
 
 function showVid () {
   var video = document.getElementById("vid")
@@ -6,5 +6,34 @@ function showVid () {
     video.style.display = "block";
   } else {
     video.style.display = "none";
+  }
+}
+
+//Donations Show/Hide Buttons
+
+function show_hide_DonateFunds() {
+  var x = document.getElementById("DonateFunds");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function show_hide_Sponsor() {
+  var x = document.getElementById("Sponsor");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function show_hide_Host() {
+  var x = document.getElementById("Host");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
